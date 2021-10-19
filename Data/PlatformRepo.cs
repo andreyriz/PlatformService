74b889601b9235ce0168b5f50e71bf289caf1fb9
a.riazantsev <andreyriz2909@gmail.com>
2021-10-20 00:39:59 +0300

    init

---
+++ Data/PlatformRepo.cs
@@ -0,0 +1,38 @@
+﻿using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            //throw new NotImplementedException();
+            _context.Platforms.Add(plat);
+        }
+
+        public IEnumerable<Platform> GetAllPlatrorm()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(pt => pt.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ Data/PrepDb.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope=app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data");
+                context.Platforms.AddRange(
+                    new Models.Platform() { Name="DotNet", Publisher="Microsoft", Cost="Free"},
+                    new Models.Platform() { Name = "Sql Service Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Models.Platform() { Name = "Kubernets", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    );
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
