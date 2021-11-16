f503244813104a764ec4da070c6a5c53014f881e
a.riazantsev <andreyriz2909@gmail.com>
2021-11-16 18:01:12 +0200

    pre_merge

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private IPlatformRepo _repository;
+        private IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms ...");
+            var platformItem = _repository.GetAllPlatrorm();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+    }
+}
