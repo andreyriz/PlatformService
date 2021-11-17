94cd403a7502e1a18ac23c4ce3a4dbb93ca3af88
a.riazantsev <andreyriz2909@gmail.com>
2021-11-17 23:40:58 +0200

    dockerfile

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,29 @@ namespace PlatformService.Controllers
             var platformItem = _repository.GetAllPlatrorm();
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem!=null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {id = platformReadDto.Id}, platformReadDto);
         }
     }
 }
